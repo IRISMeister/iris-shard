e67a33225a90c9ce6658f6bb252c36ce4a768e70
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-01-23 15:02:05 +0900

    getfile

--- getfile.sh
+++ getfile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD -o isc-taxi-demo/taxi_zones.csv
+
 curl https://data.cityofnewyork.us/api/views/kxp8-n2sj/rows.csv?accessType=DOWNLOAD -o isc-taxi-demo/2020_Yellow_Taxi_Trip_Data.csv
 
 head isc-taxi-demo/2020_Yellow_Taxi_Trip_Data.csv -n 100001 > isc-taxi-demo/100K.csv
